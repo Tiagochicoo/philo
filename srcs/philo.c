ed9f1b3b22efeca9705ec7e9eb1cff8de22cc619
tpereira <tpereira@student.42lisboa.com>
2022-07-03 21:21:47 +0100

    added pthread_detatch & removed pthread_join

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:19:22 by tpereira          #+#    #+#             */
-/*   Updated: 2022/07/03 19:22:46 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/07/03 21:20:58 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,20 @@
 
 typedef struct s_data
 {
-	int				num;
 	int				i;
-	pthread_t		*philo;
+	int				num;
+	int				forks;
+	pthread_t		*philos;
 	pthread_mutex_t	*lock;
 
 }			t_data;
 
-
-
 void	*eat(t_data *data)
 {
-	if (!data->lock)
-		printf("Philosopher %d is waiting!\n", data->i);
+	//printf("Philosopher %d is waiting!\n", data->i);
 	pthread_mutex_lock(data->lock);
 	printf("Philosopher %d is eating spaghetti!\n", data->i);
+	printf("Philosopher %d put down forks!\n", data->i);
 	pthread_mutex_unlock(data->lock);
 	return (NULL);
 }
@@ -38,24 +37,31 @@ int main(int argc, char **argv)
 	int		 	i;
 	t_data		data;
 
-	if (argv[1])
+	if (argc > 4)
 	{
 		i = 0;
 		data.num = atoi(argv[1]);
-		data.philo = malloc(sizeof(pthread_t) * data.num);
+		data.forks = data.num;
+		data.philos = malloc(sizeof(pthread_t) * data.num);
 		pthread_mutex_init(data.lock, NULL);
-	}
-	if (argc > 4)
-	{
-		while (i <= data.num)
+		while (i < data.num)
 		{
 			data.i = i;
-			pthread_create(&data.philo[i], NULL, (void *)&eat, &data);
-			pthread_join(data.philo[i], NULL);
+			pthread_create(&data.philos[i], NULL, (void *)&eat, &data);
+			pthread_detach(data.philos[i]);
 			i++;
 		}
+		// while (i < data.num)
+		// {
+		// 	pthread_join(data.philos[i], NULL);
+		// 	i++;
+		// }
+		pthread_exit(0);
+		pthread_mutex_destroy(data.lock);
+		free(data.philos);
 	}
 	else
 		printf("Usage: \"./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\"\n");
+	
 	return (0);
 }
