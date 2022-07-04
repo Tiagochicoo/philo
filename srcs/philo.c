323984ac93417f15e8606ea9d65d9048b9d63a7d
tpereira <tpereira@student.42lisboa.com>
2022-07-04 22:21:54 +0100

    debugging data->i

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:19:22 by tpereira          #+#    #+#             */
-/*   Updated: 2022/07/04 21:36:25 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/07/04 22:20:28 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,10 @@
 // 	int		right_fork;
 // } 				philo;
 
-typedef struct s_data
-{
-	int					i;
-	int					num;
-	int					forks;
-	pthread_t			*philos;
-	pthread_mutex_t		*mutex;
-}			t_data;
-
 void	*eat(t_data *data)
 {
-	printf("Philosopher %d is waiting!\n", data->i);
+	printf("data->i = %d\n", data->i);
+	//printf("Philosopher %d is waiting!\n", data->i);
 	pthread_mutex_lock(data->mutex);
 	printf("Philosopher %d is eating spaghetti!\n", data->i);
 	//printf("Philosopher %d put down forks!\n", data->i);
@@ -53,8 +45,11 @@ int main(int argc, char **argv)
 		pthread_mutex_init(data.mutex, NULL);
 		while (i < data.num)
 		{
+			printf("1 - i = %d\n", i);
 			data.i = i;
+			printf("2 - data.i = %d\n", data.i);
 			pthread_create(&data.philos[i], NULL, (void *)&eat, &data);
+			printf("3 - i = %d\n", i);
 			i++;
 		}
 		i = 0;
