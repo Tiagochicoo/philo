9b744286125c55a8596e5dd0dd2410199b685be7
tpereira <tpereira@student.42lisboa.com>
2022-07-05 22:10:40 +0100

    save

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,19 +6,11 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:19:22 by tpereira          #+#    #+#             */
-/*   Updated: 2022/07/05 17:29:15 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/07/05 22:10:27 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
-
-// typedef struct philo
-// {
-// 	clock_t	start_eating;
-// 	clock_t	finish_eating;
-// 	int		left_fork;
-// 	int		right_fork;
-// } 				philo;
 
 typedef struct s_data
 {
@@ -27,7 +19,23 @@ typedef struct s_data
 	int					forks;
 	pthread_t			*philos;
 	pthread_mutex_t		*mutex;
+	struct timeval		start;
+	struct timeval		end;
 }			t_data;
+
+typedef struct s_philo
+{
+	clock_t	start_eating;
+	clock_t	finish_eating;
+	clock_t	start_sleeping;
+	clock_t	finish_sleeping;
+	clock_t	start_thinking;
+	clock_t	finish_thinking;
+	int		meals;
+	pthread_t	thread;
+	t_data		*data;
+
+} 				t_philo;
 
 void	*eat(t_data *data)
 {
@@ -41,9 +49,9 @@ void	*eat(t_data *data)
 
 int main(int argc, char **argv)
 {
-	int		 			i;
-	t_data				data;
-
+	int				i;
+	t_data			data;
+	
 	if (argc > 4)
 	{
 		i = 0;
@@ -55,8 +63,8 @@ int main(int argc, char **argv)
 		while (i < data.num)
 		{
 			data.i = i;
+			printf("pthread_create -> %d\n", data.i);
 			pthread_create(&data.philos[i], NULL, (void *)&eat, &data);
-			printf("created thread -> %d\n", data.i);
 			i++;
 		}
 		i = 0;
@@ -64,7 +72,9 @@ int main(int argc, char **argv)
 		{
 			data.i = i;
 			pthread_join(data.philos[i], NULL);
-			printf("joined thread -> %d\n", data.i);
+			//printf("joined thread -> %d\n", data.i);
+			gettimeofday(&data.start, NULL);
+			printf("%.0f %d finished eating\n", (double)(data.start.tv_sec * 0.0001), i);
 			i++;
 		}
 		//pthread_exit(0);
@@ -73,6 +83,5 @@ int main(int argc, char **argv)
 	}
 	else
 		printf("Usage: \"./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\"\n");
-	
 	return (0);
 }
