ad32da79a603ee685b1bfdb824a8ed094908c1a2
tpereira <tpereira@student.42lisboa.com>
2022-07-07 16:31:49 +0100

    working timestamps

--- srcs/philo.c
+++ srcs/philo.c
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/02 16:19:22 by tpereira          #+#    #+#             */
-/*   Updated: 2022/07/06 21:45:38 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/07/07 16:30:14 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,61 +19,67 @@ typedef struct s_philo
 	int				right_fork;
 } 				t_philo;
 
-typedef struct s_data
+typedef struct s_info
 {
 	int					i;
 	int					num;
 	int					forks;
-	double				time_to_eat;
+	int					time_to_die;
+	int					time_to_eat;
+	int					time_to_sleep;
+	int					must_eat;
 	pthread_t			*philos;
 	pthread_mutex_t		*mutex;
-}			t_data;
+}			t_info;
 
-void	*eat(t_data *data)
+void	*eat(t_info *info)
 {
 	struct timeval	start;
 	struct timeval	end;
+	double			milliseconds;
 
+	get_forks(t_info *info);
 	gettimeofday(&start, NULL);
-	pthread_mutex_lock(data->mutex);
-	usleep(50000);
+	pthread_mutex_lock(info->mutex);
+	usleep(info->time_to_eat);
 	gettimeofday(&end, NULL);
-	printf("%.0f %d is eating\n", (double)(end.tv_sec * 1000 + end.tv_usec / 1000) - (double)(start.tv_sec * 1000 + start.tv_usec / 1000), data->i);
-	pthread_mutex_unlock(data->mutex);
+	milliseconds = (double)(end.tv_sec * 1000 + end.tv_usec / 1000) - (double)(start.tv_sec * 1000 + start.tv_usec / 1000);
+	printf("%.0f %d is eating\n", milliseconds, info->i);
+	pthread_mutex_unlock(info->mutex);
 	return (NULL);
 }
 
 int main(int argc, char **argv)
 {
 	int		 			i;
-	t_data				data;
+	t_info				info;
 
 	if (argc > 4)
 	{
 		i = 0;
-		data.num = atoi(argv[1]);
-		data.i = i;
-		data.forks = data.num;
-		data.philos = malloc(sizeof(pthread_t) * data.num);
-		pthread_mutex_init(data.mutex, NULL);
-		while (i < data.num)
+		info.num = atoi(argv[1]);
+		info.time_to_die = atoi(argv[2]) * 1000;
+		info.time_to_eat = atoi(argv[3]) * 1000;
+		info.time_to_sleep = atoi(argv[4]) * 1000;
+		info.i = i;
+		info.forks = info.num;
+		info.philos = malloc(sizeof(pthread_t) * info.num);
+		pthread_mutex_init(info.mutex, NULL);
+		while (i < info.num)
 		{
-			data.i = i;
-			pthread_create(&data.philos[i], NULL, (void *)&eat, &data);
+			info.i = i;
+			pthread_create(&info.philos[i], NULL, (void *)&eat, &info);
 			i++;
 		}
 		i = 0;
-		while (i < data.num)
+		while (i < info.num)
 		{
-			data.i = i;
-			pthread_join(data.philos[i], NULL);
-			//gettimeofday(&data.end, NULL);
-			//printf("%.0f %d finished eating\n", (double)(data.end.tv_sec * 1000 + data.end.tv_usec / 1000) - (double)(data.start.tv_sec * 1000 + data.start.tv_usec / 1000), i);
+			info.i = i;
+			pthread_join(info.philos[i], NULL);
 			i++;
 		}
-		//pthread_exit(0);
-		pthread_mutex_destroy(data.mutex);
-		free(data.philos);
+		pthread_mutex_destroy(info.mutex);
+		free(info.philos);
 	}
 	else
 		printf("Usage: \"./philo number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\"\n");
