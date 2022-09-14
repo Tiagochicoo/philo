554f2bb0f4bb3621644fd79e9df7bc5872380ba2
tpereira <tpereira@student.42lisboa.com>
2022-09-14 20:02:56 +0100

    no errors for now - working on death

--- srcs/starve.c
+++ srcs/starve.c
@@ -6,7 +6,7 @@
 /*   By: tpereira <tpereira@42Lisboa.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/23 20:27:32 by tpereira          #+#    #+#             */
-/*   Updated: 2022/09/14 19:27:22 by tpereira         ###   ########.fr       */
+/*   Updated: 2022/09/14 20:02:18 by tpereira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@ int	has_starved(t_philo *philo)
 		pthread_mutex_lock(&philo->info->death_lock);
 		philo->info->philo_died = 1;
 		pthread_mutex_unlock(&philo->info->death_lock);
-		stop_meal(philo->info);
 		return (1);
 	}
 	return (0);
@@ -56,5 +55,4 @@ void	check_death_meals(t_philo *philo)
 {
 	printf("philo->info->philo_died [%d]\n", philo->info->philo_died);
 	print_msg("died", philo, RED);
-	stop_meal(philo->info);
 }
